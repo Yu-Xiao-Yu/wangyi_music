00c014fcdca213a2687e97aaba20b304d7d1aab5
yuxiaoyu <1830421836@qq.com>
2022-09-01 22:23:45 +0800

    modified:   README.md 	modified:   package-lock.json 	modified:   package.json 	new file:   public/js/rem.js 	modified:   src/App.vue 	deleted:    src/components/HelloWorld.vue 	new file:   src/components/home/TopNav.vue 	modified:   src/main.js 	new file:   src/router/index.js 	new file:   src/store/index.js 	new file:   src/views/Home.vue 	new file:   src/views/HomeView.vue

---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="home">
+    <top-nav/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import TopNav from '@/components/home/TopNav.vue'
+export default {
+  components: { TopNav },
+    // eslint-disable-next-line vue/multi-word-component-names
+    name: 'home' ,
+    component:{
+        TopNav
+    }
+}
+</script>
+
+<style>
+
+</style>
