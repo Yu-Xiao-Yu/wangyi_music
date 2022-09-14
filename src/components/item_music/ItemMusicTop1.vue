a0beed1192ecde90ba5d121a3ed2fd5115154941
yuxiaoyu <1830421836@qq.com>
2022-09-14 12:13:43 +0800

    modified:   src/components/item_music/ItemMusicTop1.vue

--- src/components/item_music/ItemMusicTop1.vue
+++ src/components/item_music/ItemMusicTop1.vue
@@ -1,6 +1,7 @@
+<!-- 练习换个排版方式 -->
 <template>
-    <div class="itemMusicTop">
-    <img :src="playlist.coverImgUrl " alt="" class="bgimg">
+  <div class="itemMusicTop">
+    <img :src="playlist.coverImgUrl" alt="" class="bgimg" />
     <div class="itemLeft">
       <svg class="icon" aria-hidden="true" @click="$router.go(-1)">
         <use xlink:href="#icon-zuojiantou" />
@@ -20,25 +21,25 @@
   <div class="detailTop" v-if="playlist.creator != null">
     <div class="itemTopContent">
       <div class="contentLeft">
-        <img :src="playlist.coverImgUrl" class="a"  alt="">
+        <img :src="playlist.coverImgUrl" class="a" alt="" />
         <div class="playCount">
           <svg class="icon" aria-hidden="true">
             <use xlink:href="#icon-gl-play-copy" />
           </svg>
-          <span>{{changeCount(playlist.playCount)}}</span>
+          <span>{{ changeCount(playlist.playCount) }}</span>
         </div>
       </div>
       <div class="contentRight">
-        <p>{{playlist.name}}</p>
+        <p>{{ playlist.name }}</p>
         <div class="right_img">
-          <img :src="playlist.creator.avatarUrl" class="b" alt="">
-          <span>{{playlist.creator.nickname}}</span>
+          <img :src="playlist.creator.avatarUrl" class="b" alt="" />
+          <span>{{ playlist.creator.nickname }}</span>
           <svg class="icon" aria-hidden="true">
             <use xlink:href="#icon-youjiantou" />
           </svg>
         </div>
         <p class="right_two">
-          <span>{{playlist.description}}</span>
+          <span>{{ playlist.description }}</span>
           <svg class="icon" aria-hidden="true">
             <use xlink:href="#icon-youjiantou" />
           </svg>
@@ -51,13 +52,13 @@
         <svg class="icon" aria-hidden="true">
           <use xlink:href="#icon-iconfontzhizuobiaozhun023110" />
         </svg>
-        <span>{{playlist.commentCount}}</span>
+        <span>{{ playlist.commentCount }}</span>
       </div>
       <div class="footerItem">
         <svg class="icon" aria-hidden="true">
           <use xlink:href="#icon-fenxiang" />
         </svg>
-        <span>{{playlist.shareCount}}</span>
+        <span>{{ playlist.shareCount }}</span>
       </div>
       <div class="footerItem">
         <svg class="icon" aria-hidden="true">
@@ -73,18 +74,17 @@
       </div>
     </div>
   </div>
-  
 </template>
 
 <script>
 export default {
   setup(props) {
-    let pic = ""
+    let pic = "";
     // 通过props进行传值，如果数据拿不到就去获取sessiostorage里面的数据
-    if(props.playlist.creator=""){
-      props.playlist = sessionStorage.getItem('itemDetail').playlist
+    if ((props.playlist.creator = "")) {
+      props.playlist = sessionStorage.getItem("itemDetail").playlist;
     }
-    
+
     function changeCount(count) {
       if (count >= 100000000) {
         return (count / 100000000).toFixed(1) + "亿";
@@ -92,14 +92,14 @@ export default {
         return (count / 10000).toFixed(0) + "万";
       }
     }
-    return{changeCount}
+    return { changeCount };
   },
   props: ["playlist"],
 };
 </script>
 
 <style lang="less" scoped>
-.itemMusicTop{
+.itemMusicTop {
   width: 100%;
   height: 1rem;
   display: flex;
@@ -107,65 +107,66 @@ export default {
   align-items: center;
   // padding: .2rem;
   position: relative;
-  .itemLeft,.itemRight{
+  .itemLeft,
+  .itemRight {
     width: 25%;
     height: 100%;
     display: flex;
-    justify-content: space-between;//两端对齐
-    align-items: center;//垂直居中
-    margin: 0 .2rem;
-    span{
-      font-size: .4rem;
+    justify-content: space-between; //两端对齐
+    align-items: center; //垂直居中
+    margin: 0 0.2rem;
+    span {
+      font-size: 0.4rem;
       color: #fff;
     }
-    .icon{
+    .icon {
       width: 0.5rem;
       height: 0.5rem;
       fill: #fff;
     }
   }
-  .bgimg{
+  .bgimg {
     width: 100%;
     height: 11rem;
     position: fixed;
     z-index: -1;
-    filter: blur(30px);//虚化
+    filter: blur(30px); //虚化
   }
 }
 
-.detailTop{
-  .itemTopContent{
+.detailTop {
+  .itemTopContent {
     width: 100%;
     height: 2rem;
     display: flex;
     align-items: center;
     margin-top: 1rem;
-    .contentLeft{
+    .contentLeft {
       position: relative;
       width: 30%;
       margin: 0 0.4rem;
-      img{
+      img {
         width: 100%;
         height: 100%;
         border-radius: 0.2rem;
       }
-      .playCountent{
-      position: absolute;
-      top: 0.05rem;
-      right: 0.2rem;
-      font-size: 0.2rem;
-      color: #fff;
-      display: flex;
-      justify-content: center;
-      align-items: center;
-      padding: 0 0.1rem 0 0.1rem;
-      border-radius: 0.1rem;
+      .playCountent {
+        position: absolute;
+        top: 0.05rem;
+        right: 0.2rem;
+        font-size: 0.2rem;
+        color: #fff;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        padding: 0 0.1rem 0 0.1rem;
+        border-radius: 0.1rem;
       }
     }
   }
 }
 
-.b{
+.b {
   width: 130px;
   height: 130px;
 }
